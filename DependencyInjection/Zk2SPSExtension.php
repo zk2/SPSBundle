57ba0ede6ae6c68ceb0fb76dad95f31a56bcb14b
Zeka <budanov.ua@gmail.com>
2016-03-24 16:25:01 +0200

    sf3

--- DependencyInjection/Zk2SPSExtension.php
+++ DependencyInjection/Zk2SPSExtension.php
@@ -60,7 +60,7 @@ class Zk2SPSExtension extends Extension implements PrependExtensionInterface
                 case 'twig':
                     $container->prependExtensionConfig(
                         $name,
-                        array('form' => array('resources' => array($this->formTemplate)))
+                        array('form_themes' => array($this->formTemplate))
                     );
                     break;
             }
